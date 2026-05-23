172944d3a2d84241967cdfb49e5f763efa921774
Rochmady <srochmady@gmail.com>
2026-05-23 17:07:11 +0700

    Composer install component

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lumera/geoip-lite',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '97eb39b3c8e878e5661dc078b02a99e750249115',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lumera/geoip-lite' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '97eb39b3c8e878e5661dc078b02a99e750249115',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
